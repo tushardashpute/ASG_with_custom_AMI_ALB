df32060739453f083059efc93c81252dcb676b24
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2022-01-09 19:40:34 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,21 +3,6 @@ provider "aws" {
   version    = "~> 2.22.0"
   region     = "us-east-2"
   assume_role {
-	role_arn     = "arn:aws:iam::556952635478:role/ec2-role-for-creating-ekscluster"
+	role_arn     = "arn:aws:iam::************:role/ec2-role-for-creating-ekscluster"
   }
-}
-
-
-module "security_group" {
-  source = "./sg"
-}
-
-module "auto_scaling_grp" {
-  source = "./asg"
-
-
-  security_groups  = [module.security_group.security_group_id]
-  instance_type    = "t2.medium"
-  key_name         = "eks_cluster"
-
 }
